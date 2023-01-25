adcbf090df8f6552cec01cbd4e654016ac72eeb3
unknown <baptiste.caron59@gmail.com>
2023-01-25 10:37:36 +0100

    tp4

--- 6-TP4/controller/execute_telechargement.php
+++ 6-TP4/controller/execute_telechargement.php
@@ -1,7 +1,22 @@
 <?php
-include_once ('../modele/function.php');
+include_once('../modele/function.php');
 session_start();
-$_GET = downloadCSV($dl);
+
+// Fichier CSV créer 
+generateCsv('../modele/Save_Backup/Backup.json');
+
+// Télechargement du fichier CSV
+$file = "../modele/Save_Backup/DL.csv";
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
 
 header('Location:../view/affichage.php');
-?>
